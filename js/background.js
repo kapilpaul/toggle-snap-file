10ee9970ebe6d7d1a78642cb2ab17527792fdec4
Kapil Paul <kapilpaul007@gmail.com>
2022-03-17 11:59:41 +0600

    Add new menu and options

--- js/background.js
+++ js/background.js
@@ -1,17 +1,46 @@
-// Create Menu.
 chrome.contextMenus.create({
   title: "Toggle .snap.js file",
-  id: "exclude_snap_file",
-  contexts: ["all"]
+  id: "toggle_snap_file",
+  contexts: ["all"],
+});
+
+chrome.contextMenus.create({
+  title: "Toggle .test.js file",
+  id: "toggle_test_file",
+  contexts: ["all"],
+});
+
+chrome.contextMenus.create({
+  title: "Toggle All Reviewed",
+  id: "toggle_all_reviewed",
+  contexts: ["all"],
 });
 
 // On click event execute script.
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: toggleSnapFile,
+    function: handleMenuItemCallback(info),
   });
 });
+
+const handleMenuItemCallback = (info) => {
+  const { menuItemId } = info;
+
+  switch (menuItemId) {
+    case "toggle_snap_file":
+      return toggleSnapFile;
+      break;
+
+    case "toggle_all_reviewed":
+      return toggleAllReviewed;
+      break;
+
+    case "toggle_test_file":
+      return toggleTestFile;
+      break;
+  }
+};
 
 // Toggle snap file.
 const toggleSnapFile = () => {
@@ -20,4 +49,18 @@ const toggleSnapFile = () => {
       item.style.display = item.style.display === "none" ? "block" : "none";
     }
   });
+};
+
+// Toggle test.js file.
+const toggleTestFile = () => {
+  document.querySelectorAll("div.js-file").forEach(function (item, index) {
+    if (item.getAttribute("data-tagsearch-path").includes(".test.js")) {
+      item.style.display = item.style.display === "none" ? "block" : "none";
+    }
+  });
+};
+
+// Toggle review chekbox.
+const toggleAllReviewed = () => {
+  document.querySelectorAll(".js-reviewed-checkbox").forEach((c) => c.click());
 };
